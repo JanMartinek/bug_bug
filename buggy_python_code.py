2a26513fe56acade99831537c0b124515b50ce7d
JanMartinek <92045754+JanMartinek@users.noreply.github.com>
2023-03-27 17:00:11 +0200

    Update buggy_python_code.py
    fix: yaml, password

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -37,13 +37,14 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
 def authenticate(password):
-    # Assert that the password is correct
-    assert password == "Iloveyou", "Invalid password!"
-    print("Successfully authenticated!")
+    if password == "Iloveyou":
+        print("Successfully authenticated!")
+    else
+        raise Exception("Invalid password!")
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
